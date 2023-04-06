0734fc03759f95ec8d600f81fa32a60a8ed7907b
harin <harin0802@naver.com>
2023-04-06 17:11:34 +0900

    feat: user, farm entity 수정

--- src/main/java/com/example/farmfarm/Entity/FarmEntity.java
+++ src/main/java/com/example/farmfarm/Entity/FarmEntity.java
@@ -1,4 +1,15 @@
 package com.example.farmfarm.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="farm")
 public class FarmEntity {
+    @Id
+    @GeneratedValue
+    private int f_id;
+
 }
