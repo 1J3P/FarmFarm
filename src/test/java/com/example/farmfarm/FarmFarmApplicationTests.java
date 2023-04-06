a2884e2905e2a69da26cfe5a450ce41a13080b3a
hyeonah <hyeona0000@naver.com>
2023-04-06 14:39:03 +0900

    project setting

---
+++ src/test/java/com/example/farmfarm/FarmFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmFarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
