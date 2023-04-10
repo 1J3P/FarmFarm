ad1df66f0663c97cd955b42e22db830736ddd76a
hyeonah <hyeona0000@naver.com>
2023-04-10 23:12:00 +0900

    feat: user all

--- src/main/java/com/example/farmfarm/Controller/UserController.java
+++ src/main/java/com/example/farmfarm/Controller/UserController.java
@@ -1,4 +1,63 @@
 package com.example.farmfarm.Controller;
 
+import com.example.farmfarm.Entity.UserEntity;
+import com.example.farmfarm.Entity.oauth.OauthToken;
+import com.example.farmfarm.Service.UserService;
+import com.example.farmfarm.config.jwt.JwtProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class UserController {
+    @Autowired
+    private UserService userService; //(1)만들어둔 UserRepository 를 @Autowired 해준다.
+
+    @Value("${KakaoAuthUrl}")
+    private String KakaoAuthUrl;
+
+    @Value("${KakaoApiKey}")
+    private String KakaoApiKey;
+
+    @Value("${RedirectURI}")
+    private String RedirectURI;
+
+    @Value("${KakaoApiUrl}")
+    private String KakaoApiUrl;
+
+    @RequestMapping(value = "/login/getKakaoAuthUrl")
+    public @ResponseBody
+    String getKakaoAuthUrl(HttpServletRequest request){
+        String reqUrl = KakaoAuthUrl + "/oauth/authorize?client_id=" + KakaoApiKey + "&redirect_uri="+ RedirectURI + "&response_type=code";
+        return reqUrl;
+    }
+
+    // 프론트에서 인가코드 받아오는 url
+    @GetMapping("/login/oauth_kakao")
+    public ResponseEntity getLogin(@RequestParam("code") String code) {
+        // 넘어온 인가 코드를 통해 access_token 발급
+        OauthToken oauthToken= userService.getAccessToken(code);
+        // 발급 받은 accessToken 으로 카카오 회원 정보 DB 저장 후 JWT 를 생성
+        //UserService 의 기존 SaveUser 메소드를 수정한다
+        String jwtToken = userService.saveUserAndGetToken(oauthToken.getAccess_token());
+        //응답 헤더의 Authorization 이라는 항목에 JWT 를 넣어준다.
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+        //JWT 가 담긴 헤더와 200 ok 스테이터스 값, "success" 라는 바디값을 ResponseEntity 에 담아 프론트 측에 전달한다.
+        return ResponseEntity.ok().headers(headers).body(null);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Object> getCurrentUser(HttpServletRequest request) {
+        UserEntity user = userService.getUser(request);
+        return ResponseEntity.ok().body(user);
+    }
 }
