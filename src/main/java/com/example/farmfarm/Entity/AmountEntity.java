56a63c0c91da8d2d0c4b687e5ab0abdd3ee7ddbc
khskys0805 <khskys0805@naver.com>
2023-05-03 10:52:23 +0900

    feat:order,amount 생성

---
+++ src/main/java/com/example/farmfarm/Entity/AmountEntity.java
@@ -0,0 +1,24 @@
+package com.example.farmfarm.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="amount")
+@NoArgsConstructor
+@Getter
+@Setter
+public class AmountEntity {
+    @Id
+    @GeneratedValue
+    @Column(name="a_id")
+    private int aId;
+    private int total;
+    private int taxFree;
+    private int vat;
+    private int point;
+    private int discount;
+}
---
+++ src/main/java/com/example/farmfarm/Entity/OrderEntity.java
@@ -0,0 +1,41 @@
+package com.example.farmfarm.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="order")
+@NoArgsConstructor
+@Getter
+@Setter
+public class OrderEntity {
+    @Id
+    @GeneratedValue
+    @Column(name="pu_id")
+    private long puId;
+    private String status;
+    private boolean isDelivery;
+    private String deliveryAddress;
+    private String deliveryDetail;
+    private String deliveryName;
+    private String deliveryPhone;
+    private Timestamp createdAt;
+    private String deliveryMemo;
+    private int quantity;
+    private int type;
+    private int auctionApId;
+    private int groupGId;
+
+    @OneToOne
+    @JoinColumn(name="product")
+    private ProductEntity product;
+
+    @ManyToOne
+    @JoinColumn(name="user")
+    private UserEntity user;
+    //private PaymentEntity payment;
+}
