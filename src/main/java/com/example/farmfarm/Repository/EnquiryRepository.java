3fe94f79ab8efb2a32dc82b681c79e464b2d321e
Choi YoungLyn <joeuni0129@gmail.com>
2023-06-04 17:45:45 +0900

    feat: #62 - 문의사항 관련 API 구현

--- src/main/java/com/example/farmfarm/Repository/EnquiryRepository.java
+++ src/main/java/com/example/farmfarm/Repository/EnquiryRepository.java
@@ -1,12 +1,15 @@
 package com.example.farmfarm.Repository;
 
 import com.example.farmfarm.Entity.EnquiryEntity;
+import com.example.farmfarm.Entity.FarmEntity;
 import com.example.farmfarm.Entity.ProductEntity;
 import com.example.farmfarm.Entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface  EnquiryRepository extends CrudRepository<EnquiryEntity, Long> {
+import java.util.List;
+
+public interface EnquiryRepository extends CrudRepository<EnquiryEntity, Long> {
     public EnquiryEntity findByeId(long eId);
-    public EnquiryEntity findAllByProduct(ProductEntity product);
-    public EnquiryEntity findAllByUser(UserEntity user);
+    public List<EnquiryEntity> findByProduct(ProductEntity product);
+    public List<EnquiryEntity> findByUser(UserEntity user);
 }
